0e54240a996f43316af0d56d57a6ffe84a35f0a3
sdilts <stuart.dilts@gmail.com>
2019-03-05 01:23:05 -0700

    Add initial working exception framework
    + restarts aren't tested yet.

---
+++ include/exceptions.h
@@ -0,0 +1,89 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <stdbool.h>
+#include <setjmp.h>
+#include <stdio.h>
+
+enum handler_result {
+	// control flow should go back
+	// to where the handler was established
+	HANDLER_ABORT,
+	// the condition has been handled and control should
+	// return to where the condition was signaled from.
+	HANDLER_HANDLED,
+	// the handler cannot take an action and another
+	// handler should be found.
+	HANDLER_PASS
+};
+
+enum restart_result {
+	RESTART_SUCCEED,
+	RESTART_FAIL,
+	RESTART_NOT_FOUND
+};
+
+struct condition {
+	const char *name;
+	const char *message;
+	const int linenum;
+	const char *filename;
+};
+
+void fprint_condition(FILE *stream, struct condition *condition);
+
+void print_condition(struct condition *condition);
+
+void destroy_condition(struct condition *condition);
+
+// if a restart func returns true, it means that the condition was successfully handled
+typedef enum restart_result (*restart_func)(struct condition* cond, const void *data);
+
+typedef enum handler_result (*handler_func)(struct condition* cond, const void *data);
+
+struct condition_restart {
+	const char* restart_name;
+	const restart_func func;
+	const void *data;
+};
+
+// adds the given restart as a valid restart:
+void register_restart(struct condition_restart *restart);
+
+void unregister_restart(struct condition_restart *restart);
+
+struct condition_handler {
+	// the name of the condition that this handler handles:
+	const char *condition_name;
+	const handler_func func;
+	jmp_buf buf;
+	const void *data;
+};
+
+#define INIT_HANDLER(to_handle, function, _data)	\
+	{ .condition_name = to_handle, .func = function, .data = _data }
+
+// register the given condition handler. If the handler decides to
+// return RESULT_ABORT, return true and set the signaled pointer to the
+// condtition that caused the abort. If a condition is given, it must
+// be freed with destroy_condition().
+void _register_handler(struct condition_handler *handler);
+
+#define REGISTER_HANDLER(handler)	\
+	_register_handler(handler);		\
+		if(setjmp((handler)->buf))
+
+void unregister_handler(struct condition_handler *handler);
+
+// If a a condition with name condtion_name is signaled, handle it with the given restart:
+enum restart_result invoke_restart(struct condition *cond, char *restart_name);
+
+void _throw_exception(char* name, char *message, const char *filename, const int linenum);
+
+#define throw(name, message) \
+	_throw_exception(name, message, __FILE__, __LINE__)
+
+#define warn(message) \
+	_throw_exception("warning", message, __FILE__, __LINE__)
+
+#endif
---
+++ src/exceptions.c
@@ -0,0 +1,156 @@
+#include <utlist.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+#include <exceptions.h>
+
+// represents one element in the handler stack:
+struct handler_entry {
+	struct condition_handler *handler;
+	struct handler_entry *next;
+};
+
+struct restart_entry {
+	struct condition_restart *restart;
+	struct restart_entry *next;
+};
+
+struct handler_entry *handlers = NULL;
+struct restart_entry *restarts = NULL;
+
+static struct condition *create_condition(char* name, char *message,
+										const char *filename, const int linenum) {
+	struct condition *condition = malloc(sizeof(struct condition));
+	condition->name = malloc(strlen(name)+1);
+	condition->message = malloc(strlen(message)+1);
+	condition->filename = malloc(strlen(filename)+1);
+	strcpy(*(char **)&condition->name, name);
+	strcpy(*(char **)&condition->message, message);
+	// we might not need to do this, as filename /should/ be a static string
+	strcpy(*(char **)&condition->filename, filename);
+	//condition->filename = (char *) filename;
+	*(int *)&condition->linenum = linenum;
+	return condition;
+}
+
+void destroy_condition(struct condition *condition) {
+	free((char *)condition->name);
+	free((char *)condition->message);
+	free((char *)condition->filename);
+	free(condition);
+}
+
+void fprint_condition(FILE* file, struct condition *cond) {
+	fprintf(file, "condition: %s @ %s:%d",cond->name, cond->filename, cond->linenum);
+}
+
+void print_condition(struct condition *condition) {
+	fprint_condition(stdout, condition);
+}
+
+void register_restart(struct condition_restart *restart)  {
+	struct restart_entry *entry = malloc(sizeof(struct restart_entry));
+	entry->restart = restart;
+	entry->next = NULL;
+	LL_PREPEND(restarts, entry);
+}
+
+void unregister_restart(struct condition_restart *restart) {
+	struct restart_entry *entry = restarts;
+	for( ; entry != NULL; entry = entry->next) {
+		if(entry->restart == restart) {
+			break;
+		}
+	}
+	if(entry) {
+		LL_DELETE(restarts, entry);
+	} else {
+		fprintf(stderr, "cannot find restart %s\n", restart->restart_name);
+	}
+}
+
+void _register_handler(struct condition_handler *handler) {
+	struct handler_entry *entry = malloc(sizeof(struct handler_entry));
+	entry->handler = handler;
+	entry->next = NULL;
+	LL_PREPEND(handlers, entry);
+}
+
+void unregister_handler(struct condition_handler *handler) {
+	struct handler_entry *entry = handlers;
+	for( ; entry != NULL; entry = entry->next) {
+		if(entry->handler == handler) {
+			break;
+		}
+	}
+	if(entry) {
+		LL_DELETE(handlers, entry);
+		free(entry);
+	} else {
+		fprintf(stderr, "Trying to unregister non-existent handler");
+	}
+}
+
+static struct restart_entry *find_restart_entry(struct restart_entry *head, char *restart_name) {
+	struct restart_entry *entry = head;
+	for( ; entry != NULL; entry = entry->next) {
+		if(strcmp(entry->restart->restart_name, restart_name) == 0) {
+			return entry;
+		}
+	}
+	return entry;
+}
+
+enum restart_result invoke_restart(struct condition *cond, char *restart_name) {
+	struct restart_entry *entry = find_restart_entry(restarts, restart_name);
+	if(entry != NULL) {
+		return entry->restart->func(cond, entry->restart->data);
+	} else {
+		return RESTART_NOT_FOUND;
+	}
+}
+
+struct handler_entry *find_handler_entry(struct handler_entry *head, char *condition_name) {
+	struct handler_entry *entry = head;
+	for( ; entry != NULL; entry = entry->next) {
+		if(strcmp(entry->handler->condition_name, condition_name) == 0) {
+			return entry;
+		}
+	}
+	return entry;
+}
+
+void _throw_exception(char *name, char *message, const char *filename, const int linenum) {
+	struct condition *cond = create_condition(name, message, filename, linenum);
+	// search for a handler:
+	struct handler_entry *canidate = handlers;
+	enum handler_result result = HANDLER_ABORT;
+	while(canidate != NULL) {
+		canidate = find_handler_entry(canidate, name);
+		if (canidate == NULL) {
+			goto none_found;
+		}
+		result = canidate->handler->func(cond, canidate->handler->data);
+		switch(result) {
+		case HANDLER_ABORT:
+            // TODO: implement finalizers
+			longjmp(canidate->handler->buf, 1);
+		case HANDLER_HANDLED:
+			destroy_condition(cond);
+			return;
+		case HANDLER_PASS:
+			canidate = canidate->next;
+			break;
+		default:
+			fprintf(stderr, "Invalid handler option: %d", result);
+			exit(1);
+		}
+	}
+ none_found:
+	// if we get here, it means we didn't find a handler:
+	fprintf(stderr, "Fatal condition: ");
+	fprint_condition(stderr, cond);
+	exit(1);
+}
